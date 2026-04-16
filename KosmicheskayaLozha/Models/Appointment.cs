58a55beca7a1d51aa25b50a4961865f601bf15c8
234989 <tregor2019@gmail.com>
2026-04-16 13:38:32 +0300

    Добавление класса Appointment

---
+++ KosmicheskayaLozha/Models/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KosmicheskayaLozha.Models
+{
+    public class Appointment
+    {
+        public int AppointmentId { get; set; }
+        public int? ClientId { get; set; }
+        public int MasterId { get; set; }
+        public int ServiceTypeId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Comment { get; set; }
+        public string Status { get; set; }
+
+        public User Client { get; set; }
+        public User Master { get; set; }
+        public ServiceType ServiceType { get; set; }
+    }
+}
